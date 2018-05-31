bde663f06878db559036acfcb9e0f70ee5fddb61
Bianca Hale <sashabtt@gmail.com>
2018-05-31 15:47:44 -0600

    model change

--- db/migrate/20180531214705_create_songs.rb
+++ db/migrate/20180531214705_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :name
+      t.integer :us_rank
+      t.integer :eu_rank
       t.belongs_to :chart, foreign_key: true
       t.belongs_to :artist, foreign_key: true
 
